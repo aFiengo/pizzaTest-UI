188325b081066c5bd599bec1484977652e5820c6
Ariel Fiengo <ariel.fiengo@truextend.com>
2023-07-19 08:15:47 -0400

    Edited the Karma config json file and the topping-list.spec

--- src/app/components/body/topping-list/topping-list.component.spec.ts
+++ src/app/components/body/topping-list/topping-list.component.spec.ts
@@ -8,21 +8,12 @@ import { ITopping } from '../../../models/topping.model';
 describe('ToppingListComponent', () => {
     let component: ToppingListComponent;
     let fixture: ComponentFixture<ToppingListComponent>;
-    let mockToppingService: jasmine.SpyObj<ToppingService>;
-    let mockSnackBar;
 
     beforeEach(async () => {
-        mockToppingService = jasmine.createSpyObj('ToppingService', ['getAllToppings', 'addTopping', 'deleteTopping']);
-        mockSnackBar = jasmine.createSpyObj(['open']);
-
         await TestBed.configureTestingModule({
-            declarations: [ToppingListComponent],
-            providers: [
-                { provide: ToppingService, useValue: mockToppingService },
-                { provide: MatSnackBar, useValue: mockSnackBar },
-            ]
+        declarations: [ ToppingListComponent ]
         })
-            .compileComponents();
+        .compileComponents();
     });
 
     beforeEach(() => {
@@ -33,18 +24,5 @@ describe('ToppingListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('should load toppings on init', () => {
-        const mockToppings: ITopping[] = [
-            { id: '1', name: 'Mushrooms' },
-            { id: '2', name: 'Pepperoni' }
-        ];
-        mockToppingService.getAllToppings.and.returnValue(of(mockToppings));
-
-        component.ngOnInit();
-
-        expect(component.toppings.length).toBe(2);
-        expect(component.toppings).toEqual(mockToppings);
     });
 });
